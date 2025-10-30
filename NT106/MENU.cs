a4f548c2e626d8b8d96befc3fd140b096a64c1f5
kiennguly <24520885@gm.uit.edu.vn>
2025-10-30 18:47:41 +0700

    update code form menu
    kien

--- NT106/MENU.cs
+++ NT106/MENU.cs
@@ -37,68 +37,12 @@ namespace plan_fighting_super_start
             if (textBox3 != null) textBox3.Text = AccountData.Level.ToString();
 
             // Làm trong suốt / style cho các control (gọi với kiểu đầy đủ để tránh mơ hồ)
-            if (buttonPlay != null) SetTransparentButton(buttonPlay);
-            if (buttonUpgradeHP != null) SetTransparentButton(buttonUpgradeHP);
-            if (buttonUpgradeDamage != null) SetTransparentButton(buttonUpgradeDamage);
-            if (buttonExit != null) SetTransparentButton(buttonExit);
-            if (button1 != null) SetTransparentButton(button1);
-
-            if (textBoxGold != null) SetTransparentTextBox(textBoxGold);
-            if (textBox1 != null) SetTransparentTextBox(textBox1);
-            if (textBox2 != null) SetTransparentTextBox(textBox2);
-            if (textBox3 != null) SetTransparentTextBox(textBox3);
-
-            if (labelWelcome != null) SetTransparentLabel(labelWelcome);
-        }
-
-        // ===== Helpers: dùng kiểu đầy đủ để diệt xung đột kiểu =====
-
-        private void SetTransparentButton(System.Windows.Forms.Button button)
-        {
-            // Style tối giản, không đòi hỏi library ngoài
-            button.FlatStyle = FlatStyle.Flat;
-            button.FlatAppearance.BorderSize = 0;
-            button.BackColor = Color.Transparent;
-            button.ForeColor = Color.FromArgb(0, 192, 192);
-            button.UseVisualStyleBackColor = false;
-
-            button.MouseEnter += (_, __) =>
-            {
-                button.ForeColor = Color.White;
-                button.FlatAppearance.BorderSize = 1;
-                button.FlatAppearance.BorderColor = Color.FromArgb(0, 192, 192);
-            };
-
-            button.MouseLeave += (_, __) =>
-            {
-                button.ForeColor = Color.FromArgb(0, 192, 192);
-                button.FlatAppearance.BorderSize = 0;
-            };
-        }
-
-        private void SetTransparentLabel(System.Windows.Forms.Label label)
-        {
-            label.BackColor = Color.Transparent;
-            label.ForeColor = Color.FromArgb(0, 192, 192);
-            label.Parent = this;
-            label.BringToFront();
-        }
-
-        private void SetTransparentTextBox(System.Windows.Forms.TextBox textBox)
-        {
-            textBox.BorderStyle = BorderStyle.FixedSingle;
-            textBox.BackColor = Color.FromArgb(20, 20, 20);
-            textBox.ForeColor = Color.FromArgb(0, 192, 192);
-        }
-
-        // ====== Handlers nút bấm (giữ nguyên tên theo Designer) ======
-
-        // Chơi solo (nút Play)
+            if (buttonPlay != null) SetBoss
         private void buttonPlay_Click(object sender, EventArgs e)
         {
             try
             {
-                var form = new GAMESOLO();
+                var form = new GAMEBOSS();
                 form.Show();
             }
             catch (Exception ex)
@@ -129,33 +73,8 @@ namespace plan_fighting_super_start
         // Nâng Damage
         private void buttonUpgradeDamage_Click(object sender, EventArgs e)
         {
-            if (AccountData.Gold >= 15)
-            {
-                AccountData.Gold -= 15;
-                AccountData.UpgradeDamage += 5;
-
-                if (textBoxGold != null) textBoxGold.Text = AccountData.Gold.ToString();
-                if (textBox2 != null) textBox2.Text = AccountData.UpgradeDamage.ToString();
-
-                try { Database.UpdateAccountData(); } catch { }
-            }
-            else
-            {
-                MessageBox.Show("Không đủ vàng để nâng Damage!");
-            }
-        }
-
-        // Thoát game
-        private void buttonExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        // Chơi với người (button1)
-        private void button1_Click(object sender, EventArgs e)
-        {
-            // TODO: mở form/phòng online nếu có
-            var form = new GAMESOLO(); // tạm mở solo để tránh crash nếu chưa có form khác
+            if (AccouRoom
+            var form = new Room(); 
             form.Show();
         }
     }
